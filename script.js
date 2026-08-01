c6fb00aba21401422bb029a18ea94a74a4398c3e
antonyy005-AY <antonyy005@gmail.com>
2026-08-01 23:18:58 +0530

    Remove unnecessary blank line in script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const popup = document.getElementById("popup");
 const surpriseBtn = document.getElementById("surpriseBtn");
 const closePopup = document.getElementById("closePopup");
-
 const mainMessage = document.getElementById("mainMessage");
 const card = document.getElementById("birthdayCard");
 const balloonBtn = document.getElementById("balloonBtn");
